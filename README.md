454aa51abeaad4b7bb1cee38fd7c53faaa63a25a
=Hovinarri <miki.pelkonen@gmail.com>
2025-10-30 14:38:34 +0200

    Refactor toc helper

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # metropolia-javascript-course
 -----
-## Table of Contents
+## 📖 Table of Contents
 
-- [module_1](#module_1)
-  - [01_hello_world](#module_1-01_hello_world)
-  - [02_prompt_and_greet](#module_1-02_prompt_and_greet)
-  - [03_three_little_integers](#module_1-03_three_little_integers)
-  - [04_sorting_hat](#module_1-04_sorting_hat)
-  - [05_leap_year](#module_1-05_leap_year)
-  - [06_square_root](#module_1-06_square_root)
-  - [07_random_dice](#module_1-07_random_dice)
+- [module_1](./module_1/)
+  - [01_hello_world](./module_1/01_hello_world/)
+  - [02_prompt_and_greet](./module_1/02_prompt_and_greet/)
+  - [03_three_little_integers](./module_1/03_three_little_integers/)
+  - [04_sorting_hat](./module_1/04_sorting_hat/)
+  - [05_leap_year](./module_1/05_leap_year/)
+  - [06_square_root](./module_1/06_square_root/)
+  - [07_random_dice](./module_1/07_random_dice/)
--- generate_toc.fish
+++ generate_toc.fish
@@ -2,10 +2,9 @@
 
 set README "README.md"
 
-# Start the README with a header
 echo "# metropolia-javascript-course" >$README
 echo ----- >>$README
-echo "## Table of Contents" >>$README
+echo "## 📖 Table of Contents" >>$README
 echo "" >>$README
 
 for module in */
@@ -14,8 +13,7 @@ for module in */
     end
 
     set module_name (string replace -r '/$' '' $module)
-    set module_anchor (string replace " " "-" (string lower $module_name))
-    echo "- [$module_name](#$module_anchor)" >>$README
+    echo "- [$module_name](./$module_name/)" >>$README
 
     for sub in $module*/
         if test $sub = "$module""node_modules/"
@@ -23,8 +21,7 @@ for module in */
         end
 
         set sub_name (string replace -r '/$' '' $sub)
-        set sub_name (string split "/" $sub_name)[-1]
-        set sub_anchor (string replace " " "-" (string lower "$module_name-$sub_name"))
-        echo "  - [$sub_name](#$sub_anchor)" >>$README
+        set sub_name_last (string split "/" $sub_name)[-1]
+        echo "  - [$sub_name_last](./$sub_name/)" >>$README
     end
 end
