bf6d1e261defd38fc6aeb945294cef0eca0e2bec
=Hovinarri <miki.pelkonen@gmail.com>
2025-11-13 18:33:01 +0200

    Refactor assignment 01mod02

--- module_2/01_reverse_order/main.js
+++ module_2/01_reverse_order/main.js
@@ -8,7 +8,6 @@ style.textContent = `
       flex-direction: column;
       align-items: center;
     }
-
     p {
       display: inline-block;
       padding: 0.75rem;
@@ -21,61 +20,76 @@ style.textContent = `
       border-radius: 1rem;
       box-shadow: 1px 3px 6px rgba(22,22,22,0.3);
     }
-
     p:nth-child(2) {
       background-color: orange;
       border: 1px inset red;
     }
 `;
-document.head.appendChild(style);
 
+Number.prototype.clamp = function (min, max) {
+  return Math.min(Math.max(this, min), max);
+};
+
+document.head.appendChild(style);
 const resultParagraph = document.createElement("p");
 body.appendChild(resultParagraph);
-
 const errorParagraph = document.createElement("p");
 body.appendChild(errorParagraph);
 
-function promptNum(promptMsg) {
-  const timeoutMs = 50;
+function promptNum(promptMsg = "Enter a number:") {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const userInput = prompt(promptMsg)?.trim();
-      if (userInput === "" || isNaN(userInput) || userInput === null)
-        reject(`Invalid input: ${userInput}. Needs to be a number.`);
-      resolve(Number(userInput));
-    }, timeoutMs);
+      if (userInput === "" || isNaN(userInput) || userInput == null)
+        return reject(`Invalid input: ${userInput}. Needs to be a number.`);
+      return resolve(Number(userInput));
+    }, 50);
   });
 }
 
 async function promptFiveTimes() {
-  const num_array = Array(5).fill(0);
-  let errorMsg = "";
-  let i = 0;
-  while (i < num_array.length) {
-    await promptNum(`Enter ${i + 1}. number`)
-      .then((promptedNumber) => {
-        if (errorMsg !== "") {
-          errorParagraph.innerText = "";
-          errorMsg = "";
-        }
-        num_array[i] = promptedNumber;
-        resultParagraph.innerText += `Index: ${i} result: ${promptedNumber}\n`;
-        i++;
-      })
-      .catch((err) => {
-        errorMsg = `Error: ${err}`;
-        console.log(errorMsg);
-        errorParagraph.innerText = errorMsg;
-      });
+  const numArray = Array(5);
+  for (let i = 0; i < numArray.length; ) {
+    try {
+      const num = await promptNum(`Enter ${i + 1}. number`);
+      numArray[i] = num;
+      resultParagraph.innerText += `Index: ${i} result: ${num}\n`;
+      errorParagraph.innerText = "";
+      i++;
+    } catch (err) {
+      errorParagraph.innerText = `Error: ${err}`;
+      console.error(err);
+    }
   }
-  return num_array;
+  return numArray;
 }
 
-promptFiveTimes().then((num_array) => {
+function handleResult(numArray) {
+  if (numArray == null || numArray.length <= 0)
+    throw new Error("Number array cannot be null or empty.");
+
+  const timeoutMS = numArray.length * 100;
+  const timeoutStep = timeoutMS / numArray.length;
   resultParagraph.innerText = "";
-  for (let i = num_array.length - 1; i >= 0; i--) {
-    const resultNum = num_array[i];
-    console.log(resultNum);
-    resultParagraph.innerText += `Array [${i}] = ${resultNum}\n`;
+  for (let i = numArray.length - 1; i >= 0; i--) {
+    setTimeout(
+      () => {
+        const num = numArray[i];
+        resultParagraph.innerText += `Array [${i}] = ${num}\n`;
+        console.log(num);
+      },
+      Number(timeoutMS - timeoutStep * i).clamp(0, timeoutMS),
+    );
   }
-});
+}
+
+promptFiveTimes()
+  .then((result) => {
+    setTimeout(() => {
+      handleResult(result);
+    }, 500);
+  })
+  .catch((err) => {
+    errorParagraph.innerText = `${err}`;
+    console.error(err);
+  });
