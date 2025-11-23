8d6dc5d9f388e712cad0ee60a699bc0d596a45fb
=Hovinarri <miki.pelkonen@gmail.com>
2025-11-23 22:39:10 +0200

    Refactor assignment 06

--- module_2/06_random_die/main.js
+++ module_2/06_random_die/main.js
@@ -1,17 +1,38 @@
 "use strict";
 
-const body = document.body;
-const unorderedList = document.createElement("ul");
-body.appendChild(unorderedList);
-
-function rollDie() {
-  return Math.floor(Math.random() * 6) + 1;
+function randInt(start, stop) {
+  return Math.floor(Math.random() * stop) + start;
 }
 
-let dieResult;
-do {
-  dieResult = rollDie();
-  const li = document.createElement("li");
-  li.textContent = dieResult;
-  unorderedList.appendChild(li);
-} while (dieResult !== 6);
+function rollDie() {
+  const start = 1,
+    stop = 6;
+  return randInt(start, stop);
+}
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function* rollDiceAsync() {
+  let result;
+  do {
+    await timeout(200);
+    result = rollDie();
+
+    yield result;
+  } while (result !== 6);
+}
+
+(async () => {
+  const ul = document.createElement("ul");
+  document.body.appendChild(ul);
+
+  const diceRoller = rollDiceAsync();
+  for await (const result of diceRoller) {
+    console.log(`Rolling result: ${result}`);
+    const li = document.createElement("li");
+    li.textContent = result;
+    ul.appendChild(li);
+  }
+})();
